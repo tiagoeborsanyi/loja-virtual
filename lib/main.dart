8e1922e809cb4fe43a87ee42b631ec9c48544eb3
Tiago Emerick <tiagoeborsanyi@gmail.com>
2022-02-21 12:27:51 -0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+
+  FirebaseFirestore.instance.collection('teste').add({'teste': 'teste'});
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
+  }
+}
